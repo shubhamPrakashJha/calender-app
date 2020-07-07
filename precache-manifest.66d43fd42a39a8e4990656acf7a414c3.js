c97cc1c7045b8c31d80291b448c7c38a8811e8d4
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2020-07-07 09:59:57 +0530

    Updates

---
+++ precache-manifest.66d43fd42a39a8e4990656acf7a414c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6339ae492624f4c59e41450ab3294b40",
+    "url": "/calender-app/index.html"
+  },
+  {
+    "revision": "54a3959cc6cf19387195",
+    "url": "/calender-app/static/css/2.b6ddee0d.chunk.css"
+  },
+  {
+    "revision": "c8f42d5acde52ec27716",
+    "url": "/calender-app/static/css/main.8f25bef8.chunk.css"
+  },
+  {
+    "revision": "54a3959cc6cf19387195",
+    "url": "/calender-app/static/js/2.ef516e2f.chunk.js"
+  },
+  {
+    "revision": "3dfeb62e21bcc1fd7d19c0bcd56acb2f",
+    "url": "/calender-app/static/js/2.ef516e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f42d5acde52ec27716",
+    "url": "/calender-app/static/js/main.7da6d794.chunk.js"
+  },
+  {
+    "revision": "58d543d5f7dbe33d5729",
+    "url": "/calender-app/static/js/runtime-main.e942444f.js"
+  }
+]);
